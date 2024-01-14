0e931e65e76c247b208621b57a2b21fd20b91775
Raza <141234596+KMORaza@users.noreply.github.com>
2024-01-15 00:41:35 +0530

    -

--- KirchhoffsDiffraction.cpp
+++ KirchhoffsDiffraction.cpp
@@ -1,1 +1,18 @@
-
+#include "KirchhoffsDiffraction.h"
+#include <cmath>
+KirchhoffsDiffraction::KirchhoffsDiffraction(double wavelength, double distance)
+    : wavelength(wavelength), distance(distance) {}
+void KirchhoffsDiffraction::calculateDiffractionPattern(const SourceAperture& source, ObservationPlane& observation) {
+    int numPoints = observation.getNumPoints();
+    double k = 2.0 * M_PI / wavelength;
+    for (int i = 0; i < numPoints; ++i) {
+        double x = static_cast<double>(i) - numPoints / 2;
+        double integral = 0.0;
+        for (int j = 0; j < numPoints; ++j) {
+            double y = static_cast<double>(j) - numPoints / 2;
+            double r = std::sqrt(x * x + y * y + distance * distance);
+            integral += source.getAmplitude(x, y) * std::exp(std::complex<double>(0.0, k * r)) / r;
+        }
+        observation.setField(i, integral);
+    }
+}
